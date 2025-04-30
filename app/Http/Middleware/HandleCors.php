c720f2b40e741c659c23d1135116f62a60d9fd28
Naufal Falah <naufalfallah@gmail.com>
2025-04-30 16:22:27 +0700

    chore: cors

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -17,9 +17,9 @@ class HandleCors
     {
         // Set CORS headers
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://janicez728.sg-host.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
     }
